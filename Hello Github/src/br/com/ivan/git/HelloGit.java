d07923e16453928e07c4d5e9f4e9434d4dd01c57
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2023-11-09 17:23:20 -0300

    my first commit

--- Hello Github/src/br/com/ivan/git/HelloGit.java
+++ Hello Github/src/br/com/ivan/git/HelloGit.java
@@ -14,7 +14,8 @@ public class HelloGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git");
+		System.out.println("Hello Git a onde vamos hoje");
+		
 
 	}
 
